34c247d5d7886c9b8b0f8f7dd93c5482d523b0da
MarioZavala <mrzavalat@alumno.uspg.edu.gt>
2020-10-12 17:28:39 -0600

    Se agregaron condicionales (bucles) al ingresar datos, falta seccion eliminar y corregir errores en condicionales de modificar

--- src/uspg/edu/ListadoAlumnos.java
+++ src/uspg/edu/ListadoAlumnos.java
@@ -67,6 +67,7 @@ public class ListadoAlumnos {
                 case 4:
                     System.out.println("\t---Eliminar---");
 
+                    eliminarDatos();
                     break;
             }
 
@@ -78,6 +79,10 @@ public class ListadoAlumnos {
     public void ingresarValor() {
 
         char ingresaRegistro = 'n';
+        boolean condicional1 = false;
+        boolean condicional2 = false;
+        boolean condicional3 = false;
+        boolean condicional4 = false;
 
         System.out.println("---------------------------------------------------");
         System.out.println("Bienvenidos a la base de datos de Escuela Nacional");
@@ -94,55 +99,59 @@ public class ListadoAlumnos {
             apellido = sc.next();
             System.out.print("Carné: ");
             carnet = sc.nextInt();
-            System.out.print("Nota Parcial Uno: ");
-            parcial_1 = sc.nextDouble();
-/*
-            //FALTA BUCLE
-            if (parcial_1 <= 0 || parcial_1 <= 20) {
 
-                System.out.println("Nota no valida, por favor ingrese una nota "
-                        + "del 0 al 20");
-
+            do {
                 System.out.print("Nota Parcial Uno: ");
                 parcial_1 = sc.nextDouble();
-            }
-*/
-            System.out.print("Nota Parcial Dos: ");
-            parcial_2 = sc.nextDouble();
-/*
-            if (parcial_2 <= 0 || parcial_2 <= 20) {
 
-                System.out.println("Nota no valida, por favor ingrese una nota "
-                        + "del 0 al 20");
+                if (parcial_1 > -1 && parcial_1 < 21) {
+                    System.out.println("++Nota validada");
+                    condicional1 = true;
+                } else {
+                    System.out.println("--Nota no valida, por favor ingrese una nota del 0 al 20");
 
+                }
+            } while (condicional1 == false);
+
+            do {
                 System.out.print("Nota Parcial Dos: ");
                 parcial_2 = sc.nextDouble();
-            }
-*/
-            System.out.print("Zona: ");
-            zona = sc.nextDouble();
-/*
-            if (zona <= 0 || zona <= 30) {
 
-                System.out.println("Nota no valida, por favor ingrese una nota "
-                        + "del 0 al 30");
+                if (parcial_2 > -1 && parcial_2 < 21) {
+                    System.out.println("++Nota validada");
+                    condicional2 = true;
+                } else {
+                    System.out.println("--Nota no valida, por favor ingrese una nota del 0 al 20");
 
-                System.out.print("Zona: ");
+                }
+            } while (condicional2 == false);
+
+            do {
+                System.out.print("Nota Zona: ");
                 zona = sc.nextDouble();
-            }
-*/
-            System.out.print("Examen Final: ");
-            examenFinal = sc.nextDouble();
-/*
-            if (examenFinal <= 0 || examenFinal <= 20) {
 
-                System.out.println("Nota no valida, por favor ingrese una nota "
-                        + "del 0 al 20");
+                if (zona > -1 && zona < 31) {
+                    System.out.println("++Nota validada");
+                    condicional3 = true;
+                } else {
+                    System.out.println("--Nota no valida, por favor ingrese una nota del 0 al 30");
 
-                System.out.print("Examen Final: ");
+                }
+            } while (condicional3 == false);
+
+            do {
+                System.out.print("Nota Examen Final: ");
                 examenFinal = sc.nextDouble();
-            }
-*/
+
+                if (examenFinal > -1 && examenFinal < 31) {
+                    System.out.println("++Nota validada");
+                    condicional4 = true;
+                } else {
+                    System.out.println("--Nota no valida, por favor ingrese una nota del 0 al 30");
+
+                }
+            } while (condicional4 == false);
+
             listAlumnos.add(new Alumno(noAlumno, nombre, apellido, carnet,
                     parcial_1, parcial_2, zona, examenFinal));
 
@@ -166,7 +175,7 @@ public class ListadoAlumnos {
         for (Alumno alumno : listAlumnos) {
 
             System.out.println(alumno.getNoAlumno() + "\t" + alumno.getNombre() + " \t" + alumno.getApellido()
-                    + "  \t\t" + alumno.getParcial_1() + "  \t\t" + alumno.getParcial_2()
+                    + "\t" + alumno.getParcial_1() + "  \t\t" + alumno.getParcial_2()
                     + "\t\t" + alumno.getZona() + "  \t" + alumno.getExamenFinal()
                     + "  \t" + alumno.getTotal() + "\n");
         }
@@ -178,107 +187,160 @@ public class ListadoAlumnos {
 
         int noAlumno2;
         int indice = 0;
-        boolean act = false;
+        boolean condicional = false;
+        boolean condicional1 = false;
+        boolean condicional2 = false;
+        boolean condicional3 = false;
+        boolean condicional4 = false;
 
-        System.out.println("Número del alumno que desea modificar");
-        noAlumno2 = sc.nextInt();
+        do {
+            System.out.print("Número del alumno que desea modificar: ");
+            noAlumno2 = sc.nextInt();
 
-        for (Alumno alumno : listAlumnos) {
+            for (Alumno alumno : listAlumnos) {
 
-            if (alumno.getNoAlumno() == noAlumno2) {
-                int opcion2;
-                act = true;
-                do {
-                    System.out.println("\t--Modificar Datos--");
-                    System.out.println("-------*MENÚ*---------");
-                    System.out.println("Seleccione la acción que desea realizar");
-                    System.out.println("1. Carné");
-                    System.out.println("2. Nombre y Apellido");
-                    System.out.println("3. Parcial Uno");
-                    System.out.println("4. Parcial Dos");
-                    System.out.println("5. Zona");
-                    System.out.println("6. Examen Final");
-                    System.out.println("7. Salir");
-                    System.out.print("--Opción: ");
-                    opcion2 = sc.nextInt();
+                if (alumno.getNoAlumno() == noAlumno2) {
+                    int opcion2;
+                    condicional = true;
 
-                    switch (opcion2) {
+                    do {
+                        System.out.println("\t--Modificar Datos--");
+                        System.out.println("-------*MENÚ*---------");
+                        System.out.println("Seleccione la acción que desea realizar");
+                        System.out.println("1. Carné");
+                        System.out.println("2. Nombre y Apellido");
+                        System.out.println("3. Parcial Uno");
+                        System.out.println("4. Parcial Dos");
+                        System.out.println("5. Zona");
+                        System.out.println("6. Examen Final");
+                        System.out.println("7. Salir");
+                        System.out.print("--Opción: ");
+                        opcion2 = sc.nextInt();
 
-                        case 1:
-                            System.out.println("\t--Carné--");
+                        switch (opcion2) {
 
-                            System.out.print("Ingrese el nuevo carné: ");
-                            carnet = sc.nextInt();
+                            case 1:
+                                System.out.println("\t--Carné--");
 
-                            listAlumnos.get(indice).setCarnet(carnet);
-                            break;
+                                System.out.print("Ingrese el nuevo carné: ");
+                                carnet = sc.nextInt();
 
-                        case 2:
+                                listAlumnos.get(indice).setCarnet(carnet);
+                                break;
 
-                            System.out.println("\t--Nombre y Apellido--");
+                            case 2:
 
-                            System.out.print("Nuevo nombre: ");
-                            nombre = sc.next();
+                                System.out.println("\t--Nombre y Apellido--");
 
-                            listAlumnos.get(indice).setNombre(nombre);
+                                System.out.print("Nuevo nombre: ");
+                                nombre = sc.next();
 
-                            System.out.print("Nuevo apellido: ");
-                            apellido = sc.next();
-                            listAlumnos.get(indice).setApellido(apellido);
-                            break;
+                                listAlumnos.get(indice).setNombre(nombre);
 
-                        case 3:
-                            System.out.println("\t---Parcial Uno--- ");
-                            System.out.print("Ingrese la nueva nota del parcial uno: ");
-                            parcial_1 = sc.nextDouble();
+                                System.out.print("Nuevo apellido: ");
+                                apellido = sc.next();
+                                listAlumnos.get(indice).setApellido(apellido);
+                                break;
 
-                            listAlumnos.get(indice).setParcial_1(parcial_1);
-                            listAlumnos.get(indice).setTotal(alumno.getParcial_1() + alumno.getParcial_2()
-                                    + alumno.getExamenFinal() + alumno.getZona());
-                            break;
+                            case 3:
+                                System.out.println("\t---Parcial Uno--- ");
 
-                        case 4:
-                            System.out.println("\t---Parcial Dos---");
+                                do {
+                                    System.out.print("Ingrese la nueva nota del parcial uno: ");
+                                    parcial_1 = sc.nextDouble();
 
-                            System.out.print("Ingrese la nueva nota del parcial dos: ");
-                            parcial_2 = sc.nextDouble();
+                                    if (parcial_1 > -1 && parcial_1 < 21) {
+                                        System.out.println("++Nota validada");
+                                        condicional1 = true;
+                                    } else {
+                                        System.out.println("--Nota no valida, por favor ingrese una nota del 0 al 20");
 
-                            listAlumnos.get(indice).setParcial_2(parcial_2);
-                            listAlumnos.get(indice).setTotal(alumno.getParcial_1() + alumno.getParcial_2()
-                                    + alumno.getExamenFinal() + alumno.getZona());
-                            break;
+                                    }
+                                } while (condicional1 == false);
 
-                        case 5:
-                            System.out.println("\t---Zona---");
+                                listAlumnos.get(indice).setParcial_1(parcial_1);
+                                listAlumnos.get(indice).setTotal(alumno.getParcial_1() + alumno.getParcial_2()
+                                        + alumno.getExamenFinal() + alumno.getZona());
+                                break;
 
-                            System.out.print("Ingrese la nueva zona: ");
-                            zona = sc.nextDouble();
+                            case 4:
+                                System.out.println("\t---Parcial Dos---");
 
-                            listAlumnos.get(indice).setZona(zona);
-                            listAlumnos.get(indice).setTotal(alumno.getParcial_1() + alumno.getParcial_2()
-                                    + alumno.getExamenFinal() + alumno.getZona());
-                            break;
+                                do {
+                                    System.out.print("Ingrese la nueva nota del parcial dos: ");
+                                    parcial_2 = sc.nextDouble();
 
-                        case 6:
-                            System.out.println("\t---Examen Final---");
+                                    if (parcial_2 > -1 && parcial_2 < 21) {
+                                        System.out.println("++Nota validada");
+                                        condicional2 = true;
+                                    } else {
+                                        System.out.println("--Nota no valida, por favor ingrese una nota del 0 al 20");
 
-                            System.out.print("Ingrese la nueva nota del examen final: ");
-                            examenFinal = sc.nextDouble();
+                                    }
+                                } while (condicional2 == false);
 
-                            listAlumnos.get(indice).setExamenFinal(examenFinal);
-                            listAlumnos.get(indice).setTotal(alumno.getParcial_1() + alumno.getParcial_2()
-                                    + alumno.getExamenFinal() + alumno.getZona());
-                            break;
-                    }
-                } while (opcion2 != 7);
+                                listAlumnos.get(indice).setParcial_2(parcial_2);
+                                listAlumnos.get(indice).setTotal(alumno.getParcial_1() + alumno.getParcial_2()
+                                        + alumno.getExamenFinal() + alumno.getZona());
+                                break;
+
+                            case 5:
+                                System.out.println("\t---Zona---");
+
+                                do {
+                                    System.out.print("Ingrese la nueva zona: ");
+                                    zona = sc.nextDouble();
+
+                                    if (zona > -1 && zona < 31) {
+                                        System.out.println("++Nota validada");
+                                        condicional3 = true;
+                                    } else {
+                                        System.out.println("--Nota no valida, por favor ingrese una nota del 0 al 30");
+
+                                    }
+                                } while (condicional3 == false);
+
+                                listAlumnos.get(indice).setZona(zona);
+                                listAlumnos.get(indice).setTotal(alumno.getParcial_1() + alumno.getParcial_2()
+                                        + alumno.getExamenFinal() + alumno.getZona());
+                                break;
+
+                            case 6:
+                                System.out.println("\t---Examen Final---");
+
+                                do {
+                                    System.out.print("Ingrese la nueva nota del examen final: ");
+                                    examenFinal = sc.nextDouble();
+
+                                    if (examenFinal > -1 && examenFinal < 31) {
+                                        System.out.println("++Nota validada");
+                                        condicional4 = true;
+                                    } else {
+                                        System.out.println("--Nota no valida, por favor ingrese una nota del 0 al 30");
+
+                                    }
+                                } while (condicional4 == false);
+
+                                listAlumnos.get(indice).setExamenFinal(examenFinal);
+                                listAlumnos.get(indice).setTotal(alumno.getParcial_1() + alumno.getParcial_2()
+                                        + alumno.getExamenFinal() + alumno.getZona());
+                                break;
+                        }
+
+                    } while (opcion2 != 7);
+
+                }
+
+                indice++;
+
             }
-            indice++;
-        }
-        if (act == false) {
-            System.out.println("Número del alumno es incorrecto");
-            //return noAlumno2;
-        }
-           // break;
+
+            if (condicional == false) {
+                System.out.println("--Número del alumno es incorrecto");
+
+            }
+
+        } while (condicional == false);
 
     }
 //-----------------------------------------------------------------------------------------------------------------------------
